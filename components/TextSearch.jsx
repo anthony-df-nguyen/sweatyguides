05c92ca60f7156a0804f2aae475b1b28d57a7a01
skifreetony <skifreetony@yahoo.com>
2021-07-27 17:26:27 -0700

    Finished text search box component to filter an array

---
+++ components/TextSearch.jsx
@@ -0,0 +1,25 @@
+import React, {useState,useEffect} from 'react'
+
+export default function TextSearch(props) {
+    const [array, updateArray] = useState([]);
+
+    //Set Array after Initial Data Fetch of Full Array is Done
+    useEffect(() => {
+        const newArray = props.array;
+        updateArray(newArray);
+    }, [props.array]);
+
+    // Filter the array when typed
+    const filterArray = (e) => {
+        const typed = e.target.value.toLowerCase();
+        const resultArray = array.filter(item => item.name.toLowerCase().includes(typed) && 1)
+        props.function(resultArray)  
+    }
+
+
+    return (
+        <div>
+            <input className="topMargin" type="text" placeholder={props.label} style={{width:"100%",}} onChange={(e)=> filterArray(e)}></input>
+        </div>
+    )
+}
--- pages/pokedex.js
+++ pages/pokedex.js
@@ -5,22 +5,35 @@ import Image from "next/image";
 import FullScreen from "components/FullScreen.jsx";
 import ReactTable from "components/ReactTable";
 import GetAllPokemon from "components/pokemon/GetAllPokemon"
+import TextSearch from "components/TextSearch";
 
 export default function Pokedex() {
  
   const [allList,updateAllList] = useState([])
+  const [filteredList,updateFilteredList] = useState(allList)
   const [selected,updateSelected] = useState([])
   const getPokemon = async (endpoint) => {
      await fetch(endpoint).then(res => res.json()).then(data => {
          updateSelected(data);
      });
   }
+
+  //After fetching the full list, set filtered list to full list to initialize
+  useEffect(() => {
+    allList.length > 0 && updateFilteredList(allList);
+  }, [allList]);
+
   return (
     <div>
       <Page title="Pokedex">
         <GetAllPokemon function={updateAllList} />
+        <TextSearch
+          label="Filter List of Pokemon"
+          array={allList}
+          function={updateFilteredList}
+        />
 
-        <div className="flexRow">
+        <div className="flexRow topMargin">
           <div className="card">
             <div>
               <h3> {selected.name && selected.name.toUpperCase()}</h3>
@@ -48,7 +61,7 @@ export default function Pokedex() {
         </div>
 
         <div className="grid4lock2 topMargin4">
-          {allList.map((row, i) => (
+          {filteredList.map((row, i) => (
             <div key={i} onClick={(e) => getPokemon(row.url)}>
               {row.name.toUpperCase()}
             </div>
