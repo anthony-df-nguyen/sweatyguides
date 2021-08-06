98bed52a57c95be1eaba4024a7fd45daae81ca2a
skifreetony <skifreetony@yahoo.com>
2021-08-05 22:00:39 -0700

    Possible spinning wheel when loading pokemon images

--- components/pokemon/GetPokeImg.jsx
+++ components/pokemon/GetPokeImg.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import Loading from "components/Loading";
 
 export default function GetPokeImg(props) {
+
   return (
     <div
       style={{
@@ -15,6 +17,7 @@ export default function GetPokeImg(props) {
         height="1"
         width="1"
         layout="responsive"
+        placeholder="/images/loading.svg"
       />
     </div>
   );
